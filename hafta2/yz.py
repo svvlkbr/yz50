9ab1e6cf6c7e3e9647b61db8848667d3e6bffcce
svvlkbr <sevvalkibar8@gmail.com>
2026-08-26 23:33:57 +0300

    hafta2 yüklendi

---
+++ hafta2/yz.py
@@ -0,0 +1,62 @@
+import math
+class Value:
+	def __init__(self,data,_children=(),_op='',label=''):
+		self.data=data
+		self.grad=0.0
+		self.backward=lambda:None
+		self._prev=set(_children)
+		self.op=_op
+		self.label=label
+	def __repr__(self):
+		return f"Value(data={self.data}"
+
+	def __add__(self,other):
+		out=Value(self.data+other.data,(self,other),'+')
+		def _backward():
+			self.grad += 1.0*out.grad
+			other.grad +=1.0*out.grad
+		out._backward=_backward
+		return out
+
+	def __mul__(self,other):
+		out=Value(self.data*other.data,(self,other),'*')
+		def_backward():
+			self.grad +=other.data*out.grad
+			other.grad += self.data*out.grad
+		out._backward=_backward
+		return out
+
+	def tanh(self):
+		x=self.data
+		t=(math.exp(2*x)-1)/(math.exp(2*x)+1)
+		out=Value(t,(self,),'tanh')
+
+		def _backward():
+			self.grad +=(1-t**2)*out.grad
+		out._backward=_backward
+
+		return out
+
+	def backward(self):
+		topo=[]
+		visited=set()
+		def build_topo(v):
+			if v not in visited:
+				visited.add(v)
+				for child in v._prev:
+					build_topo(child)
+				topo.append(v)
+		build_topo(self)
+
+		self.grad=1.0
+		for node in reversed (topo):
+			node._backward()
+
+a=Value(2.0,label='a')
+b=Value(-3.0,label='b')
+c=Value(10.0,label='c')
+e=a*b;e.label='e'
+d=e+c;d.label='d'
+f=Value(-2.0,label='f')
+L=d*f;L.label='L'
+L.backward()
